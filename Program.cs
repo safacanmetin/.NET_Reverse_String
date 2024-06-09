7dc09ba2797f4bd3ee126479b993b566e6d57c51
safacanmetin <safacanmetin@gmail.com>
2024-06-09 17:30:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string you want to reverse:");
+            string text = Console.ReadLine();
+
+            string reversedText ="";
+
+            for (int i = text.Length - 1; i >= 0; i--) {
+                reversedText += text[i];
+            }
+            Console.WriteLine("Reversed text: " + reversedText);
+        }
+
+    }
+}
